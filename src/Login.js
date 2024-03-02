f54b3fea24d954eba9871e6e0984ee6a38a1ff71
steffindhas177@gmail.com <146445015+steffindhas@users.noreply.github.com>
2024-03-02 16:07:24 +0530

    added

--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,7 @@ function Login(){
    return (
     <div className='container'>
         <div className='leftContainer'>
-            <h1>WELCOME BACK!</h1>
+            <h2>WELCOME BACK!</h2>
             <h6>To keep contaccted with us, 
                 please login with your personal information.
             </h6>
@@ -12,7 +12,15 @@ function Login(){
 
         <div className='rightContainer'>
             <form className='formLeft'>
-                
+              <label>Email or Phone Number</label>
+              <input type='text'></input>
+              <label>Password</label>
+              <input type='Password'></input>
+              <div className='button-container'>
+              <button type='button'>Sign in</button> 
+              <button type='button'>Sign up</button>
+              </div>
+              <button className='button3'>Use Search Id</button>
 
             </form>
         </div>
