5557f3332ab87405b0d2985417054be303a7674d
Kailas M <kkailas113@gmail.com>
2024-03-02 11:52:56 +0800

    file added

---
+++ src/Login.js
@@ -0,0 +1,22 @@
+import './Login.css';
+
+function Login(){
+   return (
+    <div className='container'>
+        <div className='leftContainer'>
+            <h1>WELCOME BACK!</h1>
+            <h6>To keep contaccted with us, 
+                please login with your personal information.
+            </h6>
+        </div>
+
+        <div className='rightContainer'>
+            <form className='formLeft'>
+                
+
+            </form>
+        </div>
+    </div>
+   );
+}
+export default Login;
