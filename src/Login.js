42aced28b0efd34765ac93f8d3264e0cbca062b8
Kailas M <kkailas113@gmail.com>
2024-03-07 23:50:25 +0800

    file added

--- src/Login.js
+++ src/Login.js
@@ -5,9 +5,7 @@ function Login(){
     <div className='container'>
         <div className='leftContainer'>
             <h2>WELCOME BACK!</h2>
-            <h6>To keep contaccted with us, 
-                please login with your personal information.
-            </h6>
+            <h6>To keep connected with us, please login with your personal information.</h6>
         </div>
 
         <div className='rightContainer'>
@@ -15,14 +13,21 @@ function Login(){
               <label>Email or Phone Number</label>
               <input type='text'></input>
               <label>Password</label>
-              <input type='Password'></input>
+              <input type='password'></input>
               <div className='button-container'>
-              <button type='button'>Sign in</button> 
-              <button type='button'>Sign up</button>
+                <button type='button'>Sign in</button> 
+                <button type='button'>Sign up</button>
               </div>
               <button className='button3'>Use Search Id</button>
-
             </form>
+        </div>
+        <div className='logoutRight'>
+            <div className='logoutContainer'>
+              <h5>ARE YOU SURE?</h5>
+              <p className='para'>You are in logout page, Do you wish to logout?<br />Are you sure you want to log out?<br />come back again.. </p>
+              <button className='logout'>LOG OUT</button>
+              <button className='cancel'>CANCEL</button>
+            </div>
         </div>
     </div>
    );
